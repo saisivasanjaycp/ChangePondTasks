2200a511b3c4bc05899bd7430f03a32f4eed9c8c
Saisivasanjay <saisivasanjaychangepond@gmail.com>
2024-12-24 09:46:21 +0530

    pyt Task-1

---
+++ PythonTask/Task_1.py
@@ -0,0 +1,7 @@
+food= ['Burger','veg-pizza','momos','chinese','garlic bread','french fries','non-veg pizaa']
+print('Food Available at Restaurant : ',food)
+print('Datatype of Food : ',type(food))
+print('Total Food Items : ',len(food))
+print('Food At Present Not Available: ',food[2:5])
+food.append('Kabab')
+print('Add one more Item : ',food)
