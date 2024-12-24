ea8275bed2e6e9f0ca11e9c37d53b2ab4d92c6c5
Saisivasanjay <saisivasanjaychangepond@gmail.com>
2024-12-24 09:55:39 +0530

    Minitask

---
+++ PythonTask/Swaptask.py
@@ -0,0 +1,12 @@
+num1 = int(input('Enter Num1= '))
+num2 = int(input('Enter Num2= '))
+num1,num2 = num2,num1
+print('Swap _1: ',num1,num2)
+
+# swap with temp
+num1 = int(input('Enter Num1= '))
+num2 = int(input('Enter Num2= '))
+temp = num1
+num1 = num2
+num2 = temp
+print('Swap _2: ',num1,num2)
---
+++ PythonTask/Tiptask.py
@@ -0,0 +1,7 @@
+expense = int(input('Enter the expense = '))
+tip = int(input('Enter the tip amt = '))
+share = int(input('Enter the share persons ='))
+total_amt = expense + tip
+Each_share = total_amt/share
+print('Total Amount spended= ',total_amt)
+print('Individual Share Amount = ',Each_share)
